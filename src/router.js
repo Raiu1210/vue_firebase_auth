ee3bac4003f31aa67a690d3b88f1f5c333264042
Raiu1210 <baio1484@gmail.com>
2019-09-24 00:00:27 +0900

    sign in validation

--- src/router.js
+++ src/router.js
@@ -3,15 +3,21 @@ import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import Signup from '@/components/Signup.vue'
 import Signin from '@/components/Signin.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
+    {
+      path: '*',
+      redirect: 'signin'
+    },
     {
       path: '/',
       name: 'Home',
       component: Home,
+      meta: { requiresAuth: true }
     },
     {
       path: '/signup',
@@ -25,3 +31,26 @@ export default new Router({
     },
   ]
 })
+
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        next()
+      } else {
+        next({
+          path: '/signin',
+          query: { redirect: to.fullPath }
+        })
+      }
+    })
+  } else {
+    next() // next() を常に呼び出すようにしてください!
+  }
+})
+
+export default router
