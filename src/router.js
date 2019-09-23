d1d833a9b27a6da5ec1778336ecb4285fdee1b80
Raiu1210 <baio1484@gmail.com>
2019-09-23 19:37:41 +0900

    file structure

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
+import Signup from '@/components/Signup.vue'
+import Signin from '@/components/Signin.vue'
 
 Vue.use(Router)
 
@@ -8,16 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Home',
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/signup',
+      name: 'Signup',
+      component: Signup,
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin,
+    },
   ]
 })
